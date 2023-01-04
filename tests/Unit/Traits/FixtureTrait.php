7590ca5ad4d193fae048f694fe668fb446d9c9e8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-03 18:14:47 -0600

    Create FixtureTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Traits/FixtureTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Config\Tests\Unit\Traits;
+
+use Ghostwriter\Config\Contract\Exception\ConfigExceptionInterface;
+use InvalidArgumentException;
+
+trait FixtureTrait
+{
+    protected function fixture(string $path): string
+    {
+        $realpath = realpath(sprintf('%s/Fixture/config.%s.php', dirname(__DIR__, 2), mb_strtolower($path)));
+
+        if ($realpath === false) {
+            throw new class(
+                'Invalid fixture path: ' . $path
+            ) extends InvalidArgumentException implements ConfigExceptionInterface {
+            };
+        }
+
+        return $realpath;
+    }
+}
