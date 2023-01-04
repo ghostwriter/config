abdebd9a8d468fb66850c96aa8743e1801e9693c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-03 18:17:29 -0600

    Update ConfigInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/ConfigInterface.php
+++ src/Contract/ConfigInterface.php
@@ -6,38 +6,30 @@ namespace Ghostwriter\Config\Contract;
 
 use ArrayAccess;
 use Countable;
-use IteratorAggregate;
-use Traversable;
 
 /**
- * @implements IteratorAggregate<string,mixed>
+ * @extends ArrayAccess<array-key,mixed>
  */
-interface ConfigInterface extends ArrayAccess, Countable, IteratorAggregate
+interface ConfigInterface extends ArrayAccess, Countable
 {
+    /** @return array<array-key,mixed> */
     public function toArray(): array;
 
     public function get(string $key, mixed $default = null): mixed;
 
     public function has(string $key): bool;
 
-    public function merge(array $options): void;
+    public function join(array $options, ?string $key = null): void;
 
-    /**
-     * Merge the given configuration with the existing configuration.
-     */
-    public function mergeFromPath(string $path, string $key): void;
+    public function merge(array $options, ?string $key = null): void;
 
     public function set(string $key, mixed $value): void;
 
     public function remove(string $key): void;
 
-    public function count(): int;
-
-    public function getIterator(): Traversable;
-
     public function append(string $key, mixed $value): void;
 
     public function prepend(string $key, mixed $value): void;
 
-    public function split(string $key): self;
+    public function wrap(string $key): self;
 }
