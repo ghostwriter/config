4e4f7e269ff91b4b12965b12d75ff219b8f17e9b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-05 09:32:16 -0600

    Update ecs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- ecs.php
+++ ecs.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
 
 use PhpCsFixer\Fixer\Import\GroupImportFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
