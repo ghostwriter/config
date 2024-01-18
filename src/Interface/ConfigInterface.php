cc86cf0d833f04abec140b4cc15b1dff2f5a9ad1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 15:13:48 -0600

    Create ConfigInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/ConfigInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Config\Interface;
+
+interface ConfigInterface
+{
+    /**
+     * @template TDefault
+     * @template TGet
+     *
+     * @param TDefault $default
+     *
+     * @return TDefault|TGet
+     */
+    public function get(string $key, mixed $default = null): mixed;
+
+    public function has(string $key): bool;
+
+    public function remove(string $key): void;
+
+    /**
+     * @template TSet
+     *
+     * @param TSet $value
+     */
+    public function set(string $key, mixed $value): void;
+
+    /**
+     * @template TValue
+     *
+     * @return array<string,TValue>
+     */
+    public function toArray(): array;
+}
