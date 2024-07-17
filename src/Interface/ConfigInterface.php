2c635773e94e1257425804482972f7baf5815d8a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 15:41:47 -0500

    Update ConfigInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/ConfigInterface.php
+++ src/Interface/ConfigInterface.php
@@ -4,28 +4,50 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Config\Interface;
 
+/**
+ * @template TKey of string
+ * @template TValue
+ */
 interface ConfigInterface
 {
     /**
+     * @template TGet of string
      * @template TDefault
-     * @template TGet
      *
+     * @param TGet     $key
      * @param TDefault $default
      *
-     * @return TDefault|TGet
+     * @return TDefault|TValue
      */
     public function get(string $key, mixed $default = null): mixed;
 
+    /**
+     * @template THas of string
+     *
+     * @param THas $key
+     */
     public function has(string $key): bool;
 
+    /**
+     * @template TRemove of string
+     *
+     * @param TRemove $key
+     */
     public function remove(string $key): void;
 
     /**
-     * @template TSet
+     * @template TSet of string
+     * @template TSetValue
      *
-     * @param TSet $value
+     * @param TSet      $key
+     * @param TSetValue $value
+     *
+     * @psalm-this-out self<TSet|TKey,TValue|TSetValue>
      */
     public function set(string $key, mixed $value): void;
 
+    /**
+     * @return array<TKey,TValue>
+     */
     public function toArray(): array;
 }
