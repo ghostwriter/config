1ff1ecd4c989dfb542af9874e8829389bac61e54
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-27 00:46:43 -0600

    Add EmptyConfigKeyException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/EmptyConfigKeyException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Config\Exception;
+
+use Ghostwriter\Config\Interface\ExceptionInterface;
+use InvalidArgumentException;
+
+final class EmptyConfigKeyException extends InvalidArgumentException implements ExceptionInterface {}
