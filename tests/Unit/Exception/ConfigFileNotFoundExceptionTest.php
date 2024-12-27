a937b6515b42ac4632184660defbdc86d9b3f0b4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-27 00:46:44 -0600

    Add ConfigFileNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/ConfigFileNotFoundExceptionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Exception;
+
+use Ghostwriter\Config\Config;
+use Ghostwriter\Config\ConfigFactory;
+use Ghostwriter\Config\Exception\ConfigFileNotFoundException;
+use Ghostwriter\Config\Interface\ExceptionInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
+
+use const DIRECTORY_SEPARATOR;
+
+use function implode;
+
+#[CoversClass(ConfigFileNotFoundException::class)]
+#[CoversClass(ConfigFactory::class)]
+#[CoversClass(Config::class)]
+final class ConfigFileNotFoundExceptionTest extends AbstractTestCase
+{
+    public function testCreateFromFile(): void
+    {
+        $path = 'invalid/file/path';
+
+        $this->expectException(ExceptionInterface::class);
+
+        $this->expectException(ConfigFileNotFoundException::class);
+
+        $this->expectExceptionMessage($path);
+
+        $this->configFile($path);
+    }
+
+    public function testCreateFromFileWithDirectoryPath(): void
+    {
+        $path = implode(DIRECTORY_SEPARATOR, ['invalid', 'directory', 'path', '']);
+
+        $this->expectException(ExceptionInterface::class);
+
+        $this->expectException(ConfigFileNotFoundException::class);
+
+        $this->expectExceptionMessage($path);
+
+        $this->configFile($path);
+    }
+}
