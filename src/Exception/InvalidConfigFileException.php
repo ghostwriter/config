9178db720b5b53527d52665110a1b001710f4bfb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-07 15:26:21 -0600

    Update InvalidConfigFileException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidConfigFileException.php
+++ src/Exception/InvalidConfigFileException.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Config\Exception;
 
-use Ghostwriter\Config\Interface\ExceptionInterface;
 use InvalidArgumentException;
 
 final class InvalidConfigFileException extends InvalidArgumentException implements ExceptionInterface {}
