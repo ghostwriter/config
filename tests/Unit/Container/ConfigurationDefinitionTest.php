97f953b0cc0056eedd2104a59aefe7d9da462917
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-04-09 23:48:15 -0500

    Refactor config extension path and update tests
    Change ConfigurationExtension to target config/ghostwriter and always merge that directory

--- tests/Unit/Container/ConfigurationDefinitionTest.php
+++ tests/Unit/Container/ConfigurationDefinitionTest.php
@@ -6,26 +6,18 @@ namespace Tests\Unit\Container;
 
 use Ghostwriter\Config\Configuration;
 use Ghostwriter\Config\Container\ConfigurationDefinition;
+use Ghostwriter\Config\Container\ConfigurationExtension;
 use Ghostwriter\Config\Interface\ConfigurationInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\Service\DefinitionInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
+use function is_a;
+
 #[CoversClass(ConfigurationDefinition::class)]
 final class ConfigurationDefinitionTest extends AbstractTestCase
 {
-    public function testInstanceOfDefinitionInterface(): void
-    {
-        self::assertTrue(
-            is_a(
-                ConfigurationDefinition::class,
-                DefinitionInterface::class,
-                true,
-            ),
-        );
-    }
-
     public function testDefinition(): void
     {
         $container = $this->createMock(ContainerInterface::class);
@@ -33,11 +25,19 @@ final class ConfigurationDefinitionTest extends AbstractTestCase
         $container
             ->expects(self::once())
             ->method('alias')
-            ->with(
-                Configuration::class,
-                ConfigurationInterface::class,
-            );
+            ->with(Configuration::class, ConfigurationInterface::class);
+
+        $container
+            ->expects(self::once())
+            ->method('extend')
+            ->with(ConfigurationInterface::class, ConfigurationExtension::class)
+            ->seal();
 
         (new ConfigurationDefinition())($container);
+    }
+
+    public function testInstanceOfDefinitionInterface(): void
+    {
+        self::assertTrue(is_a(ConfigurationDefinition::class, DefinitionInterface::class, true));
     }
 }
